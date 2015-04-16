eae5252593d3c7e9f5eacc3e5200e9ef82477c7e
Wolfram Soyka <wolfram.soyka@student.tgm.ac.at>
2015-04-16 16:27:55 +0200

    .

--- DezSys08_SOA.py
+++ DezSys08_SOA.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'DezSys08_SOA is ur toll und macht mega viel spaß.'
 
 
 if __name__ == '__main__':
